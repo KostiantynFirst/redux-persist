90da97713c734adc4e61b345905d9a3f15d58e5d
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-30 00:19:21 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist'
+import { persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from 'redux-persist'
 import { persistClicksReducer } from './valueSlice'; 
 
 export const store = configureStore({
     reducer: {
         clicks: persistClicksReducer,
     },
+    middleware (getDefaultMiddleware) {
+        return getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
 
 export const persistor = persistStore(store);
